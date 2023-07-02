8d34b92257d462e3fa2a93054ad248eb59f19ecf
lxxs13 <luis.quiroz90@gmail.com>
2023-07-02 16:34:17 -0600

    Hooks, agregar, editar, eliminar pacientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    const obtenerLocalStorage = () => { 
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS);
+    }
+
+    obtenerLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter(px => px.id !== id);
+    setPacientes(pacientesActualizados);
+  }
+
   return (
     <div className="container mx-auto mt-20">
       <Header />
       <div className="mt-12 md:flex">
-        <Formulario />
-        <ListadoPacientes />
+        <Formulario pacientes={pacientes} paciente={paciente} setPacientes={setPacientes} setPaciente={setPaciente} />
+        <ListadoPacientes pacientes={pacientes} setPaciente={setPaciente} eliminarPaciente={eliminarPaciente} />
       </div>
     </div>
   )
